4db9f9a3c16defa28b3e3453a08e170059b73d8b
Lucas Matias Almendros (Trifol) <lucas.almendros@clovinn.com>
2021-01-24 17:00:04 -0300

    property atowired

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor.");
     }
+
+    //    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
@@ -22,5 +27,10 @@ public class TennisCoach implements Coach{
     public String getDailyFortune() {
         return fortuneService.getFortune();
     }
-    
+
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method.");
+//        this.fortuneService = fortuneService;
+//    }
 }
